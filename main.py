ae78cd73c18a2930a927fe4be618bc9f760c7f19
ikhsan <ikhsansudiramubaroq2910@gmail.com>
2024-10-25 02:57:49 +0700

    menambahkan deskripsi 2

--- main.py
+++ main.py
@@ -2,15 +2,29 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
+# instance objek col1 dan col2
 col1, col2 = st.columns(2)
 
+# objek 1
 with col1:
     st.image("images/photo.png")
 
+# objek 2
 with col2:
     st.title("Ikhsan Sudira Mubaroq")
     content = """
     Hi, Saya ikhsan sudira mubaroq dari Universitas Bina Sarana Informatika dan saya adalah programmer python
-    saya bekerja di mana saja dan kapan saja, tes
+    saya bekerja di mana saja dan kapan saja,Hi, Saya ikhsan sudira mubaroq dari Universitas Bina Sarana Informatika dan saya adalah programmer python
+    saya bekerja di mana saja dan kapan saja,Hi, Saya ikhsan sudira mubaroq dari Universitas Bina Sarana Informatika dan saya adalah programmer python
+    saya bekerja di mana saja dan kapan saja,Hi, Saya ikhsan sudira mubaroq dari Universitas Bina Sarana Informatika dan saya adalah programmer python
+    saya bekerja di mana saja dan kapan saja,Hi, Saya ikhsan sudira mubaroq dari Universitas Bina Sarana Informatika dan saya adalah programmer python
+    saya bekerja di mana saja dan kapan saja,Hi, Saya ikhsan sudira mubaroq dari Universitas Bina Sarana Informatika dan saya adalah programmer python
+    saya bekerja di mana saja dan kapan saja,Hi, Saya ikhsan sudira mubaroq dari Universitas Bina Sarana Informatika dan saya adalah programmer python
+    saya bekerja di mana saja dan kapan saja,
     """
     st.info(content)
+
+content2 = """
+informasi lebih lanjut silahkan hubungi saya saja, saya akan melayani anda lebih lanjut !
+"""
+st.write(content2)
