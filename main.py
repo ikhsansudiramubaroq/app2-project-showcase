fe33e99f1505e35194410fac0bf1cad2843f9bc5
ikhsan <ikhsansudiramubaroq2910@gmail.com>
2024-10-28 02:13:50 +0700

    menambahkan deskripsi,image dan url

--- main.py
+++ main.py
@@ -32,8 +32,8 @@ informasi lebih lanjut silahkan hubungi saya saja, saya akan melayani anda lebih
 """
 st.write(content2)
 
-# membagi 2 bagian kolom secara horizontal
-col3, col4 = st.columns(2)
+# membagi 3 bagian kolom secara horizontal
+col3, kolom_kosong,col4 = st.columns([1.5, 0.5, 1.5])
 
 # fungsi pandas membaca file csv dan menggunakan fungsi 'separator' yaitu pemisah antar kolom
 df = pandas.read_csv("data.csv", sep=";")
@@ -42,8 +42,19 @@ with col3:
     # pengulanan for dari 0-10 baris dan diulangi menggunakan header
     for index,row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        # images yaitu folder
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     # pengulanan for dari 10-11 baris dan diulangi menggunakan header
     for index,row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        # images yaitu folder
+        st.image("images/" + row["image"])
+        # perbedaan source code jika tidak ditambah []
+        st.write(f"Source Code ({row['url']})")
+
+
